f105c4f3efbf371c01bf91b0934fb7e6749e2209
Study <gashkovayuliua1119@mail.ru>
2023-11-25 16:22:30 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 15936;
+        int y = 20;
+        int x = i/y;
+
+        System.out.println(x);
+
+    }
+}
