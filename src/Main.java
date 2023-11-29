97a90f5683f6cf282db344dc06c8b0def34f4e68
Study <gashkovayuliua1119@mail.ru>
2023-11-29 19:30:36 +0800

    changed src

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        int price = 15936;
-        int min = 20;
-        int miles = price / min;
+        BonusMilesService service = new BonusMilesService();
+        service.calculate(10000);
+        int price = 10_000;
+        int miles = service.calculate(price);
 
         System.out.println(miles);
 
